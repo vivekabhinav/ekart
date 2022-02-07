70d0cebbb9b4765c5782a86baf26271e4199605c
vivek <vivekabhinav7@gmail.com>
2022-02-07 19:21:37 +0530

    E-Commerce application

---
+++ app/src/main/java/com/app/ekart/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.app.ekart.Prevalent;
+
+import com.app.ekart.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
